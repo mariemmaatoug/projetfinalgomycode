72e9409e87c085fa4f2d696cc9ef9cc833099eed
mariemmaatoug <maatougmariem00@gmail.com>
2021-07-20 23:00:25 +0100

    finished news

--- js/projet.js
+++ js/projet.js
@@ -12,25 +12,5 @@
 					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
 						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
 						elem.href = url + (url.indexOf('?')) }}}}}
-(function() {
-const heart = document.getElementById('heart');
-heart.addEventListener('click', function() {
-heart.classList.toggle('red');
-});
-})();
-(function() {
-const heart = document.getElementById('heart1');
-heart.addEventListener('click', function() {
-heart.classList.toggle('red');
-});
-})();(function() {
-const heart = document.getElementById('heart2');
-heart.addEventListener('click', function() {
-heart.classList.toggle('red');
-});
-})();(function() {
-const heart = document.getElementById('heart3');
-heart.addEventListener('click', function() {
-heart.classList.toggle('red');
-});
-})();
+
+
