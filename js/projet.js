e27946c69b6abf818d09ed5a4276a0c965315ed7
mariemmaatoug <maatougmariem00@gmail.com>
2021-07-21 12:39:42 +0100

    finished my profile

--- js/projet.js
+++ js/projet.js
@@ -1,16 +1,12 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-	if ('WebSocket' in window) {
-		(function () {
-			function refreshCSS() {
-				var sheets = [].slice.call(document.getElementsByTagName("link"));
-				var head = document.getElementsByTagName("head")[0];
-				for (var i = 0; i < sheets.length; ++i) {
-					var elem = sheets[i];
-					var parent = elem.parentElement || head;
-					parent.removeChild(elem);
-					var rel = elem.rel;
-					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
-						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
-						elem.href = url + (url.indexOf('?')) }}}}}
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
 
-
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+						
